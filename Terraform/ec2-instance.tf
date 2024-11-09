58295e861006952421ab09ea9a498ac1976629c4
revanthmallampalli <revanth.learn@gmail.com>
2024-11-09 19:39:58 +0530

    ec2 modified

--- Terraform/ec2-instance.tf
+++ Terraform/ec2-instance.tf
@@ -7,21 +7,21 @@ resource "aws_security_group" "ec2-sg" {
   description = "Allow inbound SSH and HTTP trafiic"
 
   ingress {
-    from_port="22"
-    to_port="22"
+    from_port=22
+    to_port=22
     protocol="tcp"
     cidr_blocks=["0,0,0,0/0"]
   }
   ingress {
-    from_port = "80"
-    to_port = "80"
+    from_port = 80
+    to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0,0,0,0/0"]
   }
 
   egress {
-    from_port = "0"
-    to_port = "0"
+    from_port = 0
+    to_port = 0
     protocol = "-1"
     cidr_blocks = ["0,0,0,0/0"]
   }
